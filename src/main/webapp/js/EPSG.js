ce2fea109cc4e5f1c3447da12e8dc3e8c61fe44e
Rafael López Torres <rafa@geowe.org>
2016-07-14 12:56:44 +0200

    added projections
    Added Canary Islands projection: EPSG:32626, EPSG:32627
    Added South Africa projection: EPSG:32733, EPSG:32734, EPSG:32735, EPSG:32736

--- src/main/webapp/js/EPSG.js
+++ src/main/webapp/js/EPSG.js
@@ -1,3 +1,11 @@
+Proj4js.defs['EPSG:3857'] = Proj4js.defs['EPSG:900913'];
 Proj4js.defs["EPSG:23030"] = "+proj=utm +zone=30 +ellps=intl +towgs84=-131,-100.3,-163.4,-1.244,-0.020,-1.144,9.39 +units=m +no_defs";
 Proj4js.defs["EPSG:25830"] = "+proj=utm +zone=30 +ellps=GRS80 +units=m +no_defs";
-Proj4js.defs['EPSG:3857'] = Proj4js.defs['EPSG:900913'];
+//Canary Islands
+Proj4js.defs["EPSG:32626"] = "+proj=utm +zone=26 +ellps=WGS84 +datum=WGS84 +units=m +no_defs";
+Proj4js.defs["EPSG:32627"] = "+proj=utm +zone=27 +ellps=WGS84 +datum=WGS84 +units=m +no_defs";
+//South Africa
+Proj4js.defs["EPSG:32733"] = "+proj=utm +zone=33 +south +ellps=WGS84 +datum=WGS84 +units=m +no_defs";
+Proj4js.defs["EPSG:32734"] = "+proj=utm +zone=34 +south +ellps=WGS84 +datum=WGS84 +units=m +no_defs";
+Proj4js.defs["EPSG:32735"] = "+proj=utm +zone=35 +south +ellps=WGS84 +datum=WGS84 +units=m +no_defs";
+Proj4js.defs["EPSG:32736"] = "+proj=utm +zone=36 +south +ellps=WGS84 +datum=WGS84 +units=m +no_defs";

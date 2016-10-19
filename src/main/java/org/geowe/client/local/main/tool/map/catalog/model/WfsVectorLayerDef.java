9faf27bca426a57f30d93a46680a87c1b0301edd
Rafael López Torres <rafa@geowe.org>
2016-10-19 12:40:40 +0200

    Update WfsVectorLayerDef.java

--- src/main/java/org/geowe/client/local/main/tool/map/catalog/model/WfsVectorLayerDef.java
+++ src/main/java/org/geowe/client/local/main/tool/map/catalog/model/WfsVectorLayerDef.java
@@ -28,7 +28,7 @@ import org.gwtopenmaps.openlayers.client.Projection;
 import org.jboss.errai.ioc.client.container.IOC;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+import com.google.gwt.http.client.URL;
 /**
  * Definicion de una capa vectorial de tipo WFS, es decir, que se construye en
  * base a una llamada a un servidor WFS.

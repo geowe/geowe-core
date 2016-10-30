3e6058b7adf7009202bca239a6b734b3fcdf90e0
Rafael López Torres <rafa@geowe.org>
2016-10-30 12:07:03 +0100

    added EPSG:25829

--- src/main/webapp/js/EPSG.js
+++ src/main/webapp/js/EPSG.js
@@ -1,6 +1,7 @@
 Proj4js.defs['EPSG:3857'] = Proj4js.defs['EPSG:900913'];
 Proj4js.defs["EPSG:23030"] = "+proj=utm +zone=30 +ellps=intl +towgs84=-131,-100.3,-163.4,-1.244,-0.020,-1.144,9.39 +units=m +no_defs";
 Proj4js.defs["EPSG:25830"] = "+proj=utm +zone=30 +ellps=GRS80 +units=m +no_defs";
+Proj4js.defs["EPSG:25829"] = "+proj=utm +zone=29 +ellps=GRS80 +units=m +no_defs";
 //Canary Islands
 Proj4js.defs["EPSG:32626"] = "+proj=utm +zone=26 +ellps=WGS84 +datum=WGS84 +units=m +no_defs";
 Proj4js.defs["EPSG:32627"] = "+proj=utm +zone=27 +ellps=WGS84 +datum=WGS84 +units=m +no_defs";

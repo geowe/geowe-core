b9a282ce8584a901eef8daf1fdbf3e94e9a7e1e7
Rafael López Torres <rafa@geowe.org>
2017-02-09 13:38:15 +0100

    Added immediate partial measurement

--- src/main/java/org/geowe/client/local/main/tool/measure/MeasureTool.java
+++ src/main/java/org/geowe/client/local/main/tool/measure/MeasureTool.java
@@ -28,6 +28,7 @@ import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
 
 import org.geowe.client.local.ImageProvider;
+import org.geowe.client.local.main.BasicToolBar;
 import org.geowe.client.local.main.map.GeoMap;
 import org.geowe.client.local.main.map.MapControlFactory;
 import org.geowe.client.local.main.tool.ToggleTool;
@@ -36,6 +37,8 @@ import org.geowe.client.local.ui.MessageDialogBuilder;
 import org.gwtopenmaps.openlayers.client.control.Measure;
 import org.gwtopenmaps.openlayers.client.event.MeasureEvent;
 import org.gwtopenmaps.openlayers.client.event.MeasureListener;
+import org.gwtopenmaps.openlayers.client.event.MeasurePartialListener;
+import org.slf4j.Logger;
 
 import com.google.inject.Inject;
 import com.sencha.gxt.core.client.Style.Side;
@@ -49,9 +52,14 @@ import com.sencha.gxt.core.client.Style.Side;
 public class MeasureTool extends ToggleTool {
 
 	@Inject
+	private Logger logger;
+	@Inject
 	private MapControlFactory mapControlFactory;
 	@Inject
 	private MessageDialogBuilder messageDialogBuilder;
+	@Inject
+	private BasicToolBar basicToolBar;
+	private Measure measure;
 
 	@Inject
 	public MeasureTool(GeoMap geoMap) {
@@ -64,15 +72,19 @@ public class MeasureTool extends ToggleTool {
 
 	@PostConstruct
 	private void initialize() {
-		Measure measure = mapControlFactory.createMesaureControl();
+		measure = mapControlFactory.createMesaureControl();
 		measure.addMeasureListener(getMeasureListener());
+		measure.addMeasurePartialListener(getPartialListener());
+
 		add(measure);
 	}
 
 	private MeasureListener getMeasureListener() {
 		return new MeasureListener() {
+			@Override
 			public void onMeasure(MeasureEvent eventObject) {
-
+				String measure = getToolBarMessage(eventObject);
+				basicToolBar.setWhat3Words(measure);
 				messageDialogBuilder
 						.createInfo(
 								UIMessages.INSTANCE.measure(),
@@ -81,12 +93,29 @@ public class MeasureTool extends ToggleTool {
 												eventObject.getMeasure(), 3),
 										eventObject.getUnits())).show();
 			}
+		};
+	}
 
-			private float getReoundedMeasure(Float measure, int decimal) {
-				BigDecimal bd = new BigDecimal(Float.toString(measure));
-				bd = bd.setScale(decimal, BigDecimal.ROUND_HALF_UP);
-				return bd.floatValue();
+	private MeasurePartialListener getPartialListener() {
+		return new MeasurePartialListener() {
+			@Override
+			public void onMeasurePartial(MeasureEvent eventObject) {
+				String partialMeasure = getToolBarMessage(eventObject);
+				basicToolBar.setWhat3Words(partialMeasure);
 			}
 		};
+	}
+
+	private float getReoundedMeasure(Float measure, int decimal) {
+		BigDecimal bd = new BigDecimal(Float.toString(measure));
+		bd = bd.setScale(decimal, BigDecimal.ROUND_HALF_UP);
+		return bd.floatValue();
+	}
+
+	private String getToolBarMessage(MeasureEvent eventObject) {
+		return UIMessages.INSTANCE.measure()
+				+ ": "
+				+ getReoundedMeasure(eventObject.getMeasure(), 3)
+				+ " " + eventObject.getUnits();
 	}
 }

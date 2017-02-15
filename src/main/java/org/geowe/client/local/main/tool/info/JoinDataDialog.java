5c776fe9364b1dd197cebe43dce5380b7b666860
Rafael López Torres <rafa@geowe.org>
2017-02-15 13:31:47 +0100

    Add files via upload

---
+++ src/main/java/org/geowe/client/local/main/tool/info/JoinDataDialog.java
@@ -0,0 +1,147 @@
+/*
+ * #%L
+ * GeoWE Project
+ * %%
+ * Copyright (C) 2015 - 2016 GeoWE.org
+ * %%
+ * This file is part of GeoWE.org.
+ * 
+ * GeoWE is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * GeoWE is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with GeoWE.  If not, see <http://www.gnu.org/licenses/>.
+ * #L%
+ */
+package org.geowe.client.local.main.tool.info;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.ApplicationScoped;
+
+import org.geowe.client.local.messages.UIMessages;
+
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.Widget;
+import com.sencha.gxt.cell.core.client.form.ComboBoxCell.TriggerAction;
+import com.sencha.gxt.data.shared.LabelProvider;
+import com.sencha.gxt.widget.core.client.Dialog;
+import com.sencha.gxt.widget.core.client.button.TextButton;
+import com.sencha.gxt.widget.core.client.container.VerticalLayoutContainer;
+import com.sencha.gxt.widget.core.client.container.VerticalLayoutContainer.VerticalLayoutData;
+import com.sencha.gxt.widget.core.client.form.FieldLabel;
+import com.sencha.gxt.widget.core.client.form.FileUploadField;
+import com.sencha.gxt.widget.core.client.form.FormPanel;
+import com.sencha.gxt.widget.core.client.form.FormPanel.Encoding;
+import com.sencha.gxt.widget.core.client.form.FormPanel.Method;
+import com.sencha.gxt.widget.core.client.form.SimpleComboBox;
+
+/**
+ * Join Data to layer dialog.
+ * 
+ * @author rafa@geowe.org
+ *
+ */
+@ApplicationScoped
+public class JoinDataDialog extends Dialog {
+
+	private FormPanel uploadPanel;
+	private SimpleComboBox<String> attributeCombo;
+	private TextButton loadFileButton;
+
+	public JoinDataDialog() {
+		super();
+		setHeadingText("Add alphanumeric data to layer");
+		setSize("420px", "250px");
+		setResizable(true);
+		setHideOnButtonClick(false);
+		setPredefinedButtons(PredefinedButton.OK, PredefinedButton.CLOSE);
+		setBodyStyleName("pad-text");
+		loadFileButton = new TextButton(UIMessages.INSTANCE.loadFiles());
+
+		initialize();
+	}
+
+	@PostConstruct
+	private void initialize() {
+		createFilePanel();
+		createAttributeComboBox();
+		add(createPanel());
+	}
+
+	public void init() {
+		attributeCombo.clear();
+		attributeCombo.setVisible(false);
+	}
+
+	private FormPanel createFilePanel() {
+		VerticalLayoutContainer layoutContainer = new VerticalLayoutContainer();
+
+		FileUploadField file = new FileUploadField();
+		file.setName(UIMessages.INSTANCE.gdidFileUploadFieldText());
+		file.setAllowBlank(false);
+
+		layoutContainer.add(new FieldLabel(file, UIMessages.INSTANCE.file()),
+				new VerticalLayoutData(-18, -1));
+		layoutContainer.add(new Label(UIMessages.INSTANCE.maxFileSizeText()),
+				new VerticalLayoutData(-18, -1));
+
+		layoutContainer.add(new FieldLabel(loadFileButton),
+				new VerticalLayoutData(-18, -1));
+
+		uploadPanel = new FormPanel();
+		uploadPanel.setMethod(Method.POST);
+		uploadPanel.setEncoding(Encoding.MULTIPART);
+		uploadPanel.setAction("fileupload.do");
+		uploadPanel.setWidget(layoutContainer);
+
+		return uploadPanel;
+	}
+
+	private void createAttributeComboBox() {
+		attributeCombo = new SimpleComboBox<String>(
+				new LabelProvider<String>() {
+					@Override
+					public String getLabel(String item) {
+						return item;
+					}
+				});
+		attributeCombo.setEnabled(false);
+		attributeCombo.setVisible(false);
+		attributeCombo.setTypeAhead(true);
+		attributeCombo.setEmptyText(UIMessages.INSTANCE
+				.asdAttributeComboEmptyText());
+		attributeCombo.setTriggerAction(TriggerAction.ALL);
+		attributeCombo.setForceSelection(true);
+		attributeCombo.setEditable(false);
+		attributeCombo.enableEvents();
+	}
+
+	private Widget createPanel() {
+		VerticalPanel vPanel = new VerticalPanel();
+		vPanel.setSpacing(2);
+		vPanel.add(uploadPanel);
+		vPanel.add(new FieldLabel(attributeCombo, "Join Attribute"));
+		return vPanel;
+	}
+
+	public SimpleComboBox<String> getAttributeCombo() {
+		return attributeCombo;
+	}
+
+	public FormPanel getUploadFormPanel() {
+		return uploadPanel;
+	}
+
+	public TextButton getLoadFileButton() {
+		return loadFileButton;
+	}
+
+}
---
+++ src/main/java/org/geowe/client/local/main/tool/info/JoinDataTool.java
@@ -0,0 +1,242 @@
+/*
+ * #%L
+ * GeoWE Project
+ * %%
+ * Copyright (C) 2015 - 2016 GeoWE.org
+ * %%
+ * This file is part of GeoWE.org.
+ * 
+ * GeoWE is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * GeoWE is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with GeoWE.  If not, see <http://www.gnu.org/licenses/>.
+ * #L%
+ */
+package org.geowe.client.local.main.tool.info;
+
+import java.util.Arrays;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
+import org.geowe.client.local.ImageProvider;
+import org.geowe.client.local.layermanager.LayerManagerWidget;
+import org.geowe.client.local.layermanager.tool.LayerTool;
+import org.geowe.client.local.layermanager.tool.create.CSV;
+import org.geowe.client.local.layermanager.tool.create.CSV.CsvItem;
+import org.geowe.client.local.main.map.GeoMap;
+import org.geowe.client.local.messages.UIMessages;
+import org.geowe.client.local.ui.ProgressBarDialog;
+import org.gwtopenmaps.openlayers.client.feature.VectorFeature;
+import org.jboss.errai.common.client.api.tasks.ClientTaskManager;
+
+import com.google.gwt.dom.client.Element;
+import com.google.gwt.resources.client.ImageResource;
+import com.google.gwt.user.client.DOM;
+import com.sencha.gxt.widget.core.client.Dialog.PredefinedButton;
+import com.sencha.gxt.widget.core.client.box.AlertMessageBox;
+import com.sencha.gxt.widget.core.client.event.SelectEvent;
+import com.sencha.gxt.widget.core.client.event.SelectEvent.SelectHandler;
+import com.sencha.gxt.widget.core.client.event.SubmitCompleteEvent;
+import com.sencha.gxt.widget.core.client.event.SubmitCompleteEvent.SubmitCompleteHandler;
+
+/**
+ * Herramienta para generar informe html de una capa
+ * 
+ * @author rafa@geowe.org
+ *
+ */
+@ApplicationScoped
+public class JoinDataTool extends LayerTool {
+
+	@Inject
+	private ClientTaskManager taskManager;
+	@Inject
+	private LayerManagerWidget layerManagerWidget;
+	@Inject
+	private JoinDataDialog joinDataDialog;
+
+	private ProgressBarDialog autoMessageBox;
+
+	protected List<CsvItem> csvItems;
+
+	@Inject
+	public JoinDataTool(LayerManagerWidget layerManagerWidget, GeoMap geoMap) {
+		super(layerManagerWidget, geoMap);
+		setText("Join Data");
+	}
+
+	@Override
+	public String getName() {
+		return "Join Data";
+	}
+
+	@Override
+	public ImageResource getIcon() {
+		return ImageProvider.INSTANCE.fileText24();
+	}
+
+	@PostConstruct
+	private void initialize() {
+		addLoadButtonListener();
+		addOkDialogListener();
+		joinDataDialog.getUploadFormPanel().addSubmitCompleteHandler(
+				getSubmitCompleteHandler());
+	}
+
+	@Override
+	public void onClick() {
+		joinDataDialog.init();
+		joinDataDialog.show();
+	}
+
+
+	private void addLoadButtonListener() {
+		joinDataDialog.getLoadFileButton().addSelectHandler(
+				new SelectHandler() {
+					@Override
+					public void onSelect(final SelectEvent event) {
+
+						taskManager.execute(new Runnable() {
+
+							@Override
+							public void run() {
+								autoMessageBox = new ProgressBarDialog(false,
+										UIMessages.INSTANCE.processing());
+								autoMessageBox.center();
+								autoMessageBox.show();
+
+								joinDataDialog.getUploadFormPanel().submit();
+							}
+						});
+					}
+				});
+	}
+
+	private SubmitCompleteHandler getSubmitCompleteHandler() {
+		return new SubmitCompleteHandler() {
+			public void onSubmitComplete(final SubmitCompleteEvent event) {
+
+				final Element label = DOM.createLabel();
+				label.setInnerHTML(event.getResults());
+
+				final String csvData = label.getInnerText();
+				if (!hasError(csvData)) {
+					CSV csv = new CSV();
+					String[] attrNames = csv.readAttributeNames(csvData);
+
+					joinDataDialog.getAttributeCombo().getStore().clear();
+					joinDataDialog.getAttributeCombo().getStore()
+							.addAll(Arrays.asList(attrNames));
+					joinDataDialog.getAttributeCombo().enable();
+					joinDataDialog.getAttributeCombo().setVisible(true);
+					csvItems = csv.getItems(csvData);
+					autoMessageBox.hide();
+				} else {
+					showAlert("Error: " + csvData);
+				}
+			}
+
+			private boolean hasError(final String contentFile) {
+				return contentFile.startsWith("413")
+						|| contentFile.startsWith("500");
+			}
+		};
+	}
+
+	private void addOkDialogListener() {
+		joinDataDialog.getButton(PredefinedButton.OK).addSelectHandler(
+				new SelectHandler() {
+					@Override
+					public void onSelect(final SelectEvent event) {
+
+						taskManager.execute(new Runnable() {
+
+							@Override
+							public void run() {
+								autoMessageBox = new ProgressBarDialog(false,
+										UIMessages.INSTANCE.processing());
+								autoMessageBox.center();
+								autoMessageBox.show();
+
+								if (existAttributeInLayer(joinDataDialog
+										.getAttributeCombo().getValue())) {
+
+									addAttributesToLayer(joinDataDialog
+											.getAttributeCombo().getStore()
+											.getAll());
+
+									addValuesToLayer(joinDataDialog
+											.getAttributeCombo().getValue());
+
+									layerManagerWidget.setSelectedLayer(
+											LayerManagerWidget.VECTOR_TAB,
+											getSelectedVectorLayer());
+
+									autoMessageBox.hide();
+									joinDataDialog.hide();
+								} else {
+									autoMessageBox.hide();
+									showAlert("El atributo seleccionado para vincular los datos debe existir en la capa");
+								}
+							}
+
+							private boolean existAttributeInLayer(
+									String attrName) {
+								return (getSelectedVectorLayer().getAttribute(
+										attrName) != null);
+							}
+
+						});
+					}
+				});
+	}
+
+	private void addAttributesToLayer(List<String> attrNames) {
+		for (String attrName : attrNames) {
+			if (getSelectedVectorLayer().getAttribute(attrName) == null) {
+				getSelectedVectorLayer().addAttribute(attrName, false);
+			}
+		}
+	}
+
+	private void addValuesToLayer(String attrNameId) {
+		for (CsvItem csvItem : csvItems) {
+			addAttributesToFeatures(attrNameId, csvItem);
+		}
+	}
+
+	private void addAttributesToFeatures(String attrNameId, CsvItem csvItem) {
+		for (VectorFeature feature : getSelectedVectorLayer().getFeatures()) {
+			if (csvItem.getValue(attrNameId).equals(
+					feature.getAttributes().getAttributeAsString(attrNameId))) {
+				addAttributesToFeature(csvItem, feature);
+				break;
+			}
+		}
+	}
+
+	private void addAttributesToFeature(CsvItem csvItem, VectorFeature feature) {
+		for (String attrName : csvItem.getAttributeNames()) {
+			feature.getAttributes().setAttribute(attrName,
+					csvItem.getValue(attrName));
+		}
+	}
+
+	private void showAlert(final String errorMsg) {
+		AlertMessageBox messageBox = new AlertMessageBox(
+				UIMessages.INSTANCE.warning(), errorMsg);
+		messageBox.show();
+	}
+
+}
